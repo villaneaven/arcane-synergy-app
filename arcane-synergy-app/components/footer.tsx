b55e187cb4b8cd13d286cbd3525e79eae9af51ea
Jose Fuentes <fuentesjosee03@gmail.com>
2026-07-23 11:01:23 -0500

    Refactor Footer component layout and styling for improved structure

--- arcane-synergy-app/components/footer.tsx
+++ arcane-synergy-app/components/footer.tsx
@@ -2,17 +2,19 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="flex h-20 w-full items-center justify-center border-t bg-background font-sans dark:bg-black gap-4">
-      <Image
-        src="https://static.wixstatic.com/media/2ccfb1_5238aad1e18c48b9bdb2843f28b5251d~mv2.png"
-        alt="Kaizen Clinical Partners Logo-2021.png"
-        height="30"
-        width="104"
-        priority
-      />
-      <p className="text-sm text-muted-foreground">
-        &copy; 2026 Kaizen Clinical Partners. All rights reserved.
-      </p>
+    <footer className="flex h-25 w-full items-center justify-center border-t bg-background font-sans dark:bg-black gap-4">
+      <div className="flex flex-col items-center gap-3">
+        <Image
+          src="https://static.wixstatic.com/media/2ccfb1_5238aad1e18c48b9bdb2843f28b5251d~mv2.png"
+          alt="Kaizen Clinical Partners Logo-2021.png"
+          height="38"
+          width="130"
+          priority
+        />
+        <p className="text-sm text-muted-foreground">
+          &copy; 2026 Kaizen Clinical Partners. All rights reserved.
+        </p>
+      </div>
     </footer>
   );
 }
