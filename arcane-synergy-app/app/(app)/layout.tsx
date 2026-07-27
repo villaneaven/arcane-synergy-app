38d4cfa3e23fa0a2a4e7f8521c5f96d907d893b8
Jose Fuentes <fuentesjosee03@gmail.com>
2026-07-27 13:20:29 -0500

    Refactor layout component: adjust flex properties for improved child rendering

--- arcane-synergy-app/app/(app)/layout.tsx
+++ arcane-synergy-app/app/(app)/layout.tsx
@@ -12,10 +12,10 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
       <SidebarProvider>
         <div className="flex min-h-screen w-full bg-background dark:bg-black">
           <AppSidebar />
-          <div className="relative flex flex-1 flex-col">
+          <div className="relative flex min-w-0 flex-1 flex-col">
             <AppSidebarTrigger />
             <main className="flex flex-1 flex-col overflow-auto">
-              <div className="flex-1">{children}</div>
+              <div className="min-w-0 flex-1">{children}</div>
               <Footer />
             </main>
           </div>
